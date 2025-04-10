efc9c6bee0264d3c078342a2dd16843734c801e8
enrperes <enrico.peressin@gmail.com>
2025-04-10 15:52:02 +0200

    query-grafici update

--- Codice/sql/query-grafici.sql
+++ Codice/sql/query-grafici.sql
@@ -1,4 +1,33 @@
---Il numero di conti cointestati per filiale che hanno almeno un prestito associato.
+--## 1. Distribuzione mensilità per i prestiti di conti con > 100.000 che sono gestiti da un gestore. ----
+CREATE OR REPLACE VIEW clienti_gestiti AS
+  SELECT cliente.id, cliente.gestore
+  FROM cliente, dipendente
+  WHERE cliente.gestore = dipendente.id;
+
+SELECT mensilità, COUNT(*)
+    FROM clienti_gestiti, possiede, conto, prestito
+    WHERE clienti_gestiti.id = possiede.cliente
+    AND possiede.conto = conto.iban
+    AND conto.saldo > 50.000
+    AND possiede.conto = prestito.conto
+    GROUP BY mensilità
+
+--## 2. soldi gestiti dai gestori in funzione dell'anzianità ----
+CREATE OR REPLACE VIEW dipendenti_gestori AS
+  SELECT dipendente.data_assunzione, dipendente.id
+  FROM cliente, dipendente
+  WHERE cliente.gestore = dipendente.id;
+
+SELECT SUM(conto.saldo) as skey, dipendenti_gestori.data_assunzione 
+  FROM clienti_gestiti, possiede, conto, dipendenti_gestori
+  WHERE clienti_gestiti.id = possiede.cliente
+  AND possiede.conto = conto.iban
+  AND dipendenti_gestori.id = clienti_gestiti.gestore 
+  GROUP BY dipendenti_gestori.data_assunzione;
+
+
+-- # 3. Per filiale, il numero di conti cointestatari con un prestito afferente ----
+
 CREATE VIEW conti_cointetsati AS
 SELECT possiede.conto, conto.filiale
 FROM possiede as p1, conto
@@ -13,3 +42,5 @@ SELECT filiale, COUNT(*)
 FROM conti_cointestati, prestito
 WHERE conti_cointestati.conto = prestito.conto
 GROUP BY filiale
+
+
